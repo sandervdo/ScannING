8b922f49dd526058c5018ecf8a3d6eef98cbdd8d
Sander van den Oever <sandervdo@gmail.com>
2016-03-22 19:05:31 +0100

    Merge 2 migrations and add foreign relations

--- backend/database/migrations/2016_03_22_155033_create_payment_requests_table.php
+++ backend/database/migrations/2016_03_22_155033_create_payment_requests_table.php
@@ -12,11 +12,22 @@ class CreatePaymentRequestsTable extends Migration
      */
     public function up()
     {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('account');
+            $table->foreign('account')->references('id')->on('accounts');
+            $table->binary('avatar');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
         Schema::create('payment_requests', function (Blueprint $table) {
             $table->increments('id');
             $table->string('description');
             $table->float('amount');
-            $table->integer('requester')->unsigned();
+            $table->unsignedInteger('requester');
+            $table->foreign('requester')->references('id')->on('clients');
             $table->string('token');
             $table->timestamps();
             $table->softDeletes();
@@ -30,6 +41,13 @@ class CreatePaymentRequestsTable extends Migration
      */
     public function down()
     {
+        Schema::table('clients', function(Blueprint $table) {
+            $table->dropForeign('clients_account_foreign');
+        });
+        Schema::table('payment_requests', function(Blueprint $table) {
+            $table->dropForeign('payment_requests_requester_foreign');
+        });
         Schema::drop('payment_requests');
+        Schema::drop('clients');
     }
 }
